2b71ffbc623214f62bf550a73eafa862e84d6cf3
hongwontae <feelchok1234@gmail.com>
2023-03-21 18:21:47 +0900

    Create PrintTest3.java

---
+++ javaproject/JavaofGod/src/chapter02/PrintTest3.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class PrintTest3 {
+
+	public static void main(String[] args) {
+		System.out.print("\n줄바꿈연습\n연습\n\n\n");
+		System.out.print("\ttab\t연습");
+		System.out.print("인용부호 \"king\" 입니다.\n");
+		System.out.print("\\ \\ \\");
+
+
+	}
+
+}
