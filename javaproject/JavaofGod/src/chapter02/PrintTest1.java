a1e5bb72b13f784b0e81f002e002592c65a8e6ff
hongwontae <feelchok1234@gmail.com>
2023-03-21 15:49:36 +0900

    Create PrintTest1.java

---
+++ javaproject/JavaofGod/src/chapter02/PrintTest1.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class PrintTest1 {
+
+	public static void main(String[] args) {
+	
+
+		//출력 메서드 : print/ 매개변수는 우리가 사용할 수 있는 다양한 변수를 전달하면 전달받고 출력한다.
+		System.out.print("안녕하세요");
+		System.out.print("반갑습니다");
+		
+		System.out.println(); //이거는 줄 바꿔준다.
+		System.out.print("이제그만");
+		System.out.println();
+		System.out.println("축구선수입니다.");
+	}
+
+}
