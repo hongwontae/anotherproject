457c432239188b0bbcda54eb48a4be78aec99679
hongwontae <feelchok1234@gmail.com>
2023-03-21 18:21:41 +0900

    Create Variable.java

---
+++ javaproject/JavaofGod/src/chapter02/Variable.java
@@ -0,0 +1,21 @@
+package chapter02;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		//변수의 선어
+		//자료형 변수아름(식별자) = 데이터;
+		
+		// 정수 1000 저장하기위한 변수를 선언하고 1000을 입력해보자.
+		int age= 100;
+		
+		// 변수 숫자- 정수, 실수 타입으로 구분
+		// 정수는 네 가지로 구분한다. (byte, short, int, long)
+		// 실수는 (float, double)
+		// 문자는 charc
+		// 논리값 boolean(true, flase)
+		// 기본적으로 8개가 있다. 
+		
+	}
+
+}
