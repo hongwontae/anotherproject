4c5e619e81d913024a05d09276c2f79facae8ae0
hongwontae <feelchok1234@gmail.com>
2023-03-21 15:46:08 +0900

    Create FirstJava.java

---
+++ javaproject/test/src/FirstJava.java
@@ -0,0 +1,11 @@
+public class FirstJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello JAVA");
+        System.out.println("asdasd");
+        System.out.println("asdasd");
+        System.out.println("asdasd");
+        System.out.println("asdasd");
+        System.out.println("asdasd");}
+    }
