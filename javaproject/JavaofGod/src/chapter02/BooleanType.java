74d6568601fd3b81d420451e0f2c2cd946d6c1bd
hongwontae <feelchok1234@gmail.com>
2023-03-21 18:21:56 +0900

    Create BooleanType.java

---
+++ javaproject/JavaofGod/src/chapter02/BooleanType.java
@@ -0,0 +1,16 @@
+package chapter02;
+
+public class BooleanType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(true);
+		System.out.println(false);
+		
+		boolean check1 = true;
+		boolean check2 = false;
+		System.out.println(check1);
+		System.out.println(check2);
+	}
+
+}
