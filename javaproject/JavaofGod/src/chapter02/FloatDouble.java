574c4bd6daa69ff370197db85342aa0691642a39
hongwontae <feelchok1234@gmail.com>
2023-03-21 18:21:52 +0900

    Create FloatDouble.java

---
+++ javaproject/JavaofGod/src/chapter02/FloatDouble.java
@@ -0,0 +1,22 @@
+package chapter02;
+
+public class FloatDouble {
+
+	public static void main(String[] args) {
+	
+		// 실수를 저장할 수 있는 타입은 float, double이다.
+		float num1 = 1.23456789F;
+		System.out.println(num1);
+		double num2 = 1.23456789123456789;
+		System.out.println(num2);
+		System.out.println("1.23456789123456789");
+		
+		float height = 180.5f;
+		System.out.println(height);
+		
+		double height2 = 180.5;
+		System.out.println(height2);
+
+	}
+
+}
