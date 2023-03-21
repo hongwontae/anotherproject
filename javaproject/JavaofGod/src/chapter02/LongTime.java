1e9852907ad4232062f1da3a14fe8dc10019e093
hongwontae <feelchok1234@gmail.com>
2023-03-21 18:21:50 +0900

    Create LongTime.java

---
+++ javaproject/JavaofGod/src/chapter02/LongTime.java
@@ -0,0 +1,17 @@
+package chapter02;
+
+public class LongTime {
+
+	public static void main(String[] args) {
+	
+		long number = 100000000000L;
+		
+		System.out.println(number);
+		
+		byte num2 = 120;
+		System.out.println(num2);
+		
+		
+	}
+
+}
