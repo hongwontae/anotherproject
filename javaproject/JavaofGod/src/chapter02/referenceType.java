4ed692cdee122b4e79ab6028cbcc42c7e50cf82b
hongwontae <feelchok1234@gmail.com>
2023-03-22 17:38:33 +0900

    Create referenceType.java

---
+++ javaproject/JavaofGod/src/chapter02/referenceType.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class referenceType {
+
+	public static void main(String[] args) {
+		String str = null;
+		// 참조변수 : 객체의 주소값이 들어간다.null은 아직 주소값을 저장하고 있지 않다.
+		
+		str = "JAVA"; //"문자열" - 스트링타입의 객체를 생성
+		System.out.println(str);
+		System.out.println(str);
+	}
+
+}
