bf5ff82d6ddadbe96a9efd0d8d4d92caeca23221
hongwontae <feelchok1234@gmail.com>
2023-03-22 18:12:38 +0900

    Update Operate15.java

--- javaproject/JavaofGod/src/chapter03/Operate15.java
+++ javaproject/JavaofGod/src/chapter03/Operate15.java
@@ -6,7 +6,10 @@ public class Operate15 {
 		
 		short snum1 = 20;
 		short snum2 = 30;
-		snum1 += snum2;
+		snum1 += snum2; //복합된 연산을 쓰여지는 타입을 일치시킨다.
+		
+//		snum1 = snum1 +snum2;
+		
 		System.out.println(snum1);
 		
 		
