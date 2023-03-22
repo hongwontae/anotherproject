d85320e998748f4f91f956745f3f83168ef129a6
hongwontae <feelchok1234@gmail.com>
2023-03-22 23:46:19 +0900

    Create Member.java

---
+++ MyProject/src/project/Member.java
@@ -0,0 +1,25 @@
+package project;
+
+public class Member {
+
+	public static void main(String[] args) {
+		
+		//Member 클래스를 생성하고, main()메소드를 만들고 자기 자신의 이름을 출력하는 메소드를 만들어 출력해 봅시다
+		System.out.println("홍원태");
+		
+		
+		String name = null;
+		int age = 0;
+		double height = 0;
+		boolean hasbook = false;
+		
+		System.out.printf("이름 : %s\n", "홍원태");
+		System.out.printf("나이 : %d\n", 26);
+		System.out.printf("키 : %f\n", 176.0);
+		System.out.printf("책 소유 여부 : %s\n", "true");
+		
+
+
+	}
+
+}
