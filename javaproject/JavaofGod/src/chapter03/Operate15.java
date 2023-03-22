4d8c18a3554e2d07800ef65c4b5fe8d362301ff9
hongwontae <feelchok1234@gmail.com>
2023-03-22 18:11:51 +0900

    Create Operate15.java

---
+++ javaproject/JavaofGod/src/chapter03/Operate15.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class Operate15 {
+
+	public static void main(String[] args) {
+		
+		short snum1 = 20;
+		short snum2 = 30;
+		snum1 += snum2;
+		System.out.println(snum1);
+		
+		
+		
+	}
+
+}
