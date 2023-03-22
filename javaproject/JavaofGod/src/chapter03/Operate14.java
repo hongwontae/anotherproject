a80edec1c9563a0f60e49d7416deab780d208f69
hongwontae <feelchok1234@gmail.com>
2023-03-22 18:02:46 +0900

    Create Operate14.java

---
+++ javaproject/JavaofGod/src/chapter03/Operate14.java
@@ -0,0 +1,22 @@
+package chapter03;
+
+public class Operate14 {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		
+		num+=5; //num= num+5와 같다.
+		System.out.println(num);
+		num-=5;
+		System.out.println(num);
+		num*=2;
+		System.out.println(num);
+		num/=2;
+		System.out.println(num);
+		num%=3;
+		System.out.println(num);
+
+	}
+
+}
