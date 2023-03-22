b7e444b24b255fb85a658d91a05d4603c9b85cdf
hongwontae <feelchok1234@gmail.com>
2023-03-22 17:38:37 +0900

    Create shapeTransition.java

---
+++ javaproject/JavaofGod/src/chapter02/shapeTransition.java
@@ -0,0 +1,25 @@
+package chapter02;
+
+public class shapeTransition {
+
+	public static void main(String[] args) {
+		
+		char c = 'a';
+		System.out.println(c);
+		
+		int n1 = 1;
+		System.out.println(n1);
+		
+		System.out.println(c+n1);
+		
+		int n2 = 10;
+		System.out.println(n2);
+		
+		float n3 = 10.0f;
+		System.out.println(n2+n3);
+		
+		
+
+	}
+
+}
