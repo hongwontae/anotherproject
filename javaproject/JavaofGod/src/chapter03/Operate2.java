f775a8c5355c0c2c3f3300b57e3a9a9f24166f2c
hongwontae <feelchok1234@gmail.com>
2023-03-22 17:38:52 +0900

    Create Operate2.java

---
+++ javaproject/JavaofGod/src/chapter03/Operate2.java
@@ -0,0 +1,19 @@
+package chapter03;
+
+public class Operate2 {
+	public static void main(String[] args) {
+		
+		
+		int num1 = 5;
+		int num2 = 13;
+		boolean led = true;
+		System.out.println("led 변수의 논리 값="+led);
+		System.out.println("led의 변수의 논리 값의 변경="+!led);
+		System.out.println("num1>num2의 결과"+ (num1>num2));
+		System.out.println("num1>num2의 결과 값을 반전 한 결과"+ !(num1>num2));
+		System.out.println("num1>num2"+ (num1>num2));
+		
+		
+	}
+
+}
