d5a47b462efdfebca4ea95d2259f5062c613aa54
hongwontae <feelchok1234@gmail.com>
2023-03-23 12:49:12 +0900

    Create Iftest03.java

---
+++ javaproject/JavaofGod/src/chapter04/Iftest03.java
@@ -0,0 +1,22 @@
+package chapter04;
+
+public class Iftest03 {
+
+	public static void main(String[] args) {
+		
+		int score;
+		score=85;
+		if(score>=90) {System.out.println("A");
+		}if(score>96) {
+			System.out.println("A+");
+		} else if(score>93) {
+			System.out.println("A");
+		}
+		else if(score>=80) {
+			System.out.println("B");}
+		else {System.out.println("C");}
+		
+
+	}
+
+}
