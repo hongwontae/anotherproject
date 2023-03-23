881b5292a9ac8f70697c5a28cb80f2022cd49795
hongwontae <feelchok1234@gmail.com>
2023-03-23 18:15:31 +0900

    Create Challenge06.java

---
+++ MyProject/src/project/Challenge06.java
@@ -0,0 +1,16 @@
+package project;
+
+public class Challenge06 {
+
+	public static void main(String[] args) {
+		
+		
+		int i =1;
+		while(i<=99) {
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
