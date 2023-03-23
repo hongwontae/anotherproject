e4efad22297b74ca2f6b00afacfe07b3fdfc653f
hongwontae <feelchok1234@gmail.com>
2023-03-23 18:15:06 +0900

    Create DoWhile.java

---
+++ javaproject/JavaofGod/src/chapter04/DoWhile.java
@@ -0,0 +1,16 @@
+package chapter04;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		//do while의 특징은 반드시 한번은 실행한다.
+		
+		int num = 0;
+		do {
+			System.out.println("A");
+			num++;
+		}while(num<5);	
+		
+	}
+
+}
