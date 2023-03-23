8e6a38c4e69d590c7855b20338c12049264c7d9a
hongwontae <feelchok1234@gmail.com>
2023-03-23 18:15:08 +0900

    Create ForLoop.java

---
+++ javaproject/JavaofGod/src/chapter04/ForLoop.java
@@ -0,0 +1,16 @@
+package chapter04;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		// 0부터 출력
+		// a를 5번 출력
+		for(int i =0; i<5; i++) {
+			System.out.println("A: \t"+i);
+		}
+		
+
+	}
+
+}
