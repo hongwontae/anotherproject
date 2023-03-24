e4c03aea20dd572bee0dfe4a704ea1c621bc3d2c
hongwontae <feelchok1234@gmail.com>
2023-03-24 17:53:31 +0900

    Update Challenge01.java

--- MyProject/src/project/Challenge01.java
+++ MyProject/src/project/Challenge01.java
@@ -3,13 +3,12 @@ package project;
 public class Challenge01 {
 
 	public static void main(String[] args) {
-		
-		int num = 120;
-		if(num>0 && num%2==0) {
+
+		int num = 123;
+		if (num > 0 && num % 2 == 0) {
 			System.out.println("양수이면서 짝수");
 		}
-		
-		
-	}
+
+	} 
 
 }
