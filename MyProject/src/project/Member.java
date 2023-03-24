6eeeb59545f8048de8c2bd38d544b845801b44a9
hongwontae <feelchok1234@gmail.com>
2023-03-24 17:53:50 +0900

    Update Member.java

--- MyProject/src/project/Member.java
+++ MyProject/src/project/Member.java
@@ -9,16 +9,25 @@ public class Member {
 		
 		
 		String name = null;
-		int age = 0;
-		double height = 0;
-		boolean hasbook = false;
+		int age = 0; // 변수 선언의 초기화(인스턴스 함수는 초기화 자동)
+		double height = 190.5;
+		boolean hasbook = true;
 		
 		System.out.printf("이름 : %s\n", "홍원태");
 		System.out.printf("나이 : %d\n", 26);
 		System.out.printf("키 : %f\n", 176.0);
-		System.out.printf("책 소유 여부 : %s\n", "true");
+		System.out.printf("책 소유 여부 : %s\n", "false");
 		
-
+		
+		////변수 선언 : 메모리 할당, 저장, 이름으로 참조한다.
+		//① String 타입의 이름을 저장할 수 있는 변수 name을 정의해봅시다.
+		//② int 타입의 나이를 저장할 수 있는 변수 age를 정의해봅시다.
+		//③ double 타입의 키를 저장할 수 있는 변수 height를 정의해봅시다.
+		//④ boolean 타입의 JAVA책의 보유 여부를 저장할 수 있는 변수 hasBook를 정의해봅시다.
+		//⑤ 이름과 나이, 키, 책의 보유 여부를 출력해봅시다.
+				
+				
+	
 
 	}
 
