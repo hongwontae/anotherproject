c4ab103c7771c11d83d9abfdfea96fcf04ff4594
hongwontae <feelchok1234@gmail.com>
2023-03-24 18:17:59 +0900

    Update Challenge06.java

--- MyProject/src/project/Challenge06.java
+++ MyProject/src/project/Challenge06.java
@@ -4,13 +4,15 @@ public class Challenge06 {
 
 	public static void main(String[] args) {
 		
-		
+
 		int i =1;
-		while(i<=99) {
+		while(i<100) {
 			System.out.println(i);
 			i++;
 		}
-		 
+	
+		
+		
+		
 	}
-
 }
