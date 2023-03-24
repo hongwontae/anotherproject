72d80100ef2211a4faf46b1dae57ee2d1fb1b1f3
hongwontae <feelchok1234@gmail.com>
2023-03-24 17:53:53 +0900

    Update Person.java

--- MyProject/src/project/Person.java
+++ MyProject/src/project/Person.java
@@ -3,10 +3,16 @@ package project;
 public class Person {
 
 	public static void main(String[] args) {
-		String userName = null;
-		int userPhone = 0;
-		long userId = 0L;
 		
+//		① 회원이름을 저장하는 변수
+//		② 회원 전화번호 ( 000-0000-0000 )를 저장하는 변수
+//		③ 회원 주민등록번호 ( 000000-0000000 또는 0000000000000 )를 저장하는 변수
+
+		String membername = "이강인";
+		String memphoneNn = "010-1111-7777";
+		String personNn = "991214-1212121";
+		long pNum = 987987987;
+		System.out.println(membername);
 		
 
 	}
