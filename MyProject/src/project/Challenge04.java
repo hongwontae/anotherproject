c5090b337c9c625be9d906a1c919fe9bd65994ba
hongwontae <feelchok1234@gmail.com>
2023-03-24 18:18:01 +0900

    Update Challenge04.java

--- MyProject/src/project/Challenge04.java
+++ MyProject/src/project/Challenge04.java
@@ -3,20 +3,20 @@ package project;
 public class Challenge04 {
 
 	public static void main(String[] args) {
-		
-		int n =3;
-		if(n==1) {
-			System.out.println("simple java");
-		} else if(n==2) {
-			System.out.println("funny java");
-		}else if(n==3) {
-			System.out.println("fantastic java");
-		}else {
-			System.out.println("the best programming language");
+
+		int n = 0;
+		if (n == 3) {
+			if (n == 1) {
+				System.out.println("simple java");
+			} else if (n == 2) {
+				System.out.println("funny java");
+			} else if (n == 3) {
+				System.out.println("fantastic java");
+			} else {
+				System.out.println("the best programming language");
+			}
 		}
-		 
-		System.out.println("Do you like coffe?");
-		
+		System.out.println("do you like coffe?");
 
 	}
 
