0238ee77f0cdc1e16d619380d0a94b026adfb631
hongwontae <feelchok1234@gmail.com>
2023-03-28 18:16:43 +0900

    Create ContactMain3.java

---
+++ MyProject/src/Contact/ContactMain3.java
@@ -0,0 +1,39 @@
+package Contact;
+
+import java.util.Scanner;
+
+public class ContactMain3 {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("전화번호 정보 입력을 시작합니다.");
+		
+		System.out.println("이름>");
+		String name = in.nextLine();
+		
+		System.out.println("전화번호>");
+		String phoneNumber = in.nextLine();
+		
+		System.out.println("이메일>");
+		String email = in.nextLine();
+		
+		System.out.println("주소>");
+		String address = in.nextLine();
+		
+		System.out.println("생일>");
+		String birthday = in.nextLine();
+		
+		System.out.println("그룹>");
+		String group = in.nextLine();
+		
+		ClassContact contact = new ClassContact(name, phoneNumber, email, address
+				, birthday, group);
+		
+		System.out.println("입력결과 =====");
+		
+		
+		
+		
+		
+	}
+}
