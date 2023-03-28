6d439dc62c7ed02bd63d6fb43dd01f6178ea4f50
hongwontae <feelchok1234@gmail.com>
2023-03-28 17:49:05 +0900

    Create ContactMain.java

---
+++ MyProject/src/Contact/ContactMain.java
@@ -0,0 +1,18 @@
+package Contact;
+
+public class ContactMain {
+
+	public static void main(String[] args) {
+		ClassContact c = new ClassContact("손흥민", "010-8489-3121",
+				"dnjsxoghd@naver.com", "런던", "20210223", "친구");
+		
+		System.out.println("이름 : "+ c.getName());
+		System.out.println("전화번호 : "+ c.getPhoneNumber());
+		System.out.println("이메일 : "+c.getEmail());
+		System.out.println("주소 : "+c.getAddress());
+		System.out.println("생일 : "+c.getBirthday());
+		System.out.println("그룹 : "+c.getGroup());
+
+	}
+
+}
