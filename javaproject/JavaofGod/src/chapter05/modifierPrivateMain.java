309362de29faf4bd719b8096c002e7233142ddd9
hongwontae <feelchok1234@gmail.com>
2023-03-28 23:53:39 +0900

    Update modifierPrivateMain.java

--- javaproject/JavaofGod/src/chapter05/modifierPrivateMain.java
+++ javaproject/JavaofGod/src/chapter05/modifierPrivateMain.java
@@ -3,7 +3,7 @@ package chapter05;
 public class modifierPrivateMain {
 	public static void main(String[] args) {
 		
-		modifierPrivate mp = new modifierPrivate("손흥민",20);
+		modifierPrivate mp = new modifierPrivate("손세이셔널",20);
 		
 //		mp.age=20;
 //		mp.name = "손흥민";
