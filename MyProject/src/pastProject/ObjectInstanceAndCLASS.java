0315b3ef769ccbe80c868552c4cbebebf6829824
hongwontae <feelchok1234@gmail.com>
2023-03-28 17:07:41 +0900

    Update ObjectInstanceAndCLASS.java

--- MyProject/src/pastProject/ObjectInstanceAndCLASS.java
+++ MyProject/src/pastProject/ObjectInstanceAndCLASS.java
@@ -2,41 +2,64 @@ package pastProject;
 
 public class ObjectInstanceAndCLASS {
 
-	String name;
-	String number;
-	String major;
-	String email;
-	String birthday;
-	String address;
+	private String name;
+	private String number;
+	private String major;
+	private int grade;
+	private String email;
+	private String birthday;
+	private String address;
 	
-	ObjectInstanceAndCLASS(String name, String number, String major, String email, String birthday, String address) {
+	
+	
+	public ObjectInstanceAndCLASS(String name, String number, String major, int grade, String email, String birthday,
+			String address) {
+//		super();
 		this.name = name;
 		this.number = number;
 		this.major = major;
+		this.grade = grade;
 		this.email = email;
 		this.birthday = birthday;
 		this.address = address;
 	}
 	
-	ObjectInstanceAndCLASS(String name, String number, String major, String email) {
+	public ObjectInstanceAndCLASS(String name, String number, String major, int grade, String email) {
+//		super();
 		this.name = name;
 		this.number = number;
 		this.major = major;
+		this.grade = grade;
 		this.email = email;
-		this.birthday = "정의하지 않겠다";
-		this.address = "정의하지 않겠다";
-
-}
-
-	void print() {
-		System.out.println("이름 : " + name);
-		System.out.println("전화번호 : " + number);
-		System.out.println("전공 : " + major);
-		System.out.println("이메일 : " + email);
-		System.out.println("생일 : " + birthday);
-		System.out.println("주소 : " + address);
 	}
 
 
+
+
+	public void print() {
+		System.out.println("이름 : " + name);
+		System.out.println("전화번호 : " + number);
+		System.out.println("전공 : " + major);
+		System.out.println("학년"+grade);
+		System.out.println("이메일 : " + email);
+//		if(birthday !=null) {
+//			System.out.println("생일 : " + birthday); //null
+//		} else {
+//			System.out.println("생일 : 미입력");
+//		}
+//		
+		System.out.println("생일 : " +(birthday != null ? birthday : "미입력"));
+		
+		
+//		if(address != null) {
+//			System.out.println("주소 : " + address);
+//		} else {
+//			System.out.println("주소 : 미입력");}
+//		}
+//	
+		System.out.println("조소 : "+ (address !=null ? address : "미입력"));
 	
+
+
+	}	
 }
