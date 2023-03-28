e418c324e6315d4ad8d35c48a61182d75635af81
hongwontae <feelchok1234@gmail.com>
2023-03-28 23:53:46 +0900

    Create PersonMain.java

---
+++ javaproject/JavaofGod/src/chapter05/PersonMain.java
@@ -0,0 +1,18 @@
+package chapter05;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+			
+		Person person = new Person();
+		
+		person.setName("손흥민");
+		person.setAge(28);
+		person.tell();
+		
+		
+		
+
+	}
+
+}
