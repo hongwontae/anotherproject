bb46ac9a73c42e66ee3027afb462ac2496ae1684
hongwontae <feelchok1234@gmail.com>
2023-03-28 16:03:41 +0900

    Update Member.java

--- javaproject/JavaofGod/src/chapter05/phone/Member.java
+++ javaproject/JavaofGod/src/chapter05/phone/Member.java
@@ -7,20 +7,24 @@ public class Member {
 	String email;
 	int age;
 	String phoneNumber;
+	SmartPhone phone; //null
 	
-	public Member(String name, String email, int age, String phoneNumber) {
+	public Member
+	(String name, String email, int age, String phoneNumber, SmartPhone phone) {
 //		super(); 상속할 때 하자
 		this.name = name;
 		this.email = email;
 		this.age = age;
 		this.phoneNumber = phoneNumber;
+		this.phone = phone;
 	}
 
 	public Member(String name, String email) {
 		this.name = name;
 		this.email = email;
 		this.age = 20;
-		this.phoneNumber = "입력데이터가 없습니다."; //기본값을 넣어주는 것이다.
+		this.phoneNumber = "입력데이터가 없습니다.";//기본값을 넣어주는 것이다.
+		this.phone = new SmartPhone();
 	}
 	
 	void showData() {
@@ -28,7 +32,9 @@ public class Member {
 		System.out.println("이메일  : "+ this.email);
 		System.out.println("나이 : "+ this.age);
 		System.out.println("전화번호 : "+ this.phoneNumber);
+		this.phone.call();
 	}
+	
 	
 	
 	
