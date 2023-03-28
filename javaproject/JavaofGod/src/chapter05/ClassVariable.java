cc78b8e6b8db5b49ca8a931758e81622014d08d3
hongwontae <feelchok1234@gmail.com>
2023-03-28 23:53:33 +0900

    Update ClassVariable.java

--- javaproject/JavaofGod/src/chapter05/ClassVariable.java
+++ javaproject/JavaofGod/src/chapter05/ClassVariable.java
@@ -3,15 +3,18 @@ package chapter05;
 public class ClassVariable {
 	static String name = "king";
 	static int age = 10;
-	
-	
-	void tellName() {									//static으로 한다 하더라도 이름만 적어도 된다.
-		System.out.println("나의 이름은 "+ name+"입니다."); //클래스 멤버도 인스턴스 처럼 같은 멤버로 인식한다.
+
+	void tellName() { // static으로 한다 하더라도 이름만 적어도 된다.
+		System.out.println("나의 이름은 " + name + "입니다."); // 클래스 멤버도 인스턴스 처럼 같은 멤버로 인식한다.
 	}
+
 	void tellAge() {
-		System.out.println("나는 "+age+"살 입니다.");
+		System.out.println("나는 " + age + "살 입니다.");
 	}
-	
+
 	public static void main(String[] args) {
+
+		System.out.println("이름" + ClassVariable.name);
+		System.out.println("나이"+ClassVariable.age);
 	}
 }
