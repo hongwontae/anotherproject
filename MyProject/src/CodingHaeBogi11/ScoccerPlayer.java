341baefa641c5dbaff49af2740889eb9c9f8c371
hongwontae <128343635+hongwontae@users.noreply.github.com>
2023-04-06 00:47:03 +0900

    Update ScoccerPlayer.java

--- MyProject/src/CodingHaeBogi11/ScoccerPlayer.java
+++ MyProject/src/CodingHaeBogi11/ScoccerPlayer.java
@@ -1,48 +1,55 @@
-package CodingHaeBogi11;
+package codingHaeBogi11;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScoccerPlayer {
-	
-		String name;
-		int number;
-		String team;
-		int age;
-		
-	public static void main(String[] args) {
-		
-		List<ScoccerPlayer> player = new ArrayList();
-		ScoccerPlayer player1 = new ScoccerPlayer();
-		ScoccerPlayer player2 = new ScoccerPlayer();
-		ScoccerPlayer player3 = new ScoccerPlayer();
-		ScoccerPlayer player4 = new ScoccerPlayer();
-		
-		player1.age = 20;
-		player1.name= "헨더슨";
-		player1.number=14;
-		player1.team = "리버풀";
-		
-		player2.age = 17;
-		player2.name= "바이셰티치";
-		player2.number=22;
-		player2.team = "리버풀";
-		
-		player3.age = 25;
-		player3.name= "벨링엄";
-		player3.number=8;
-		player3.team = "리버풀";
-		
-		player4.age = 40;
-		player4.name= "제라드";
-		player4.number=88;
-		player4.team = "LEGEND OF LENGEND 제라드";
-		
-		
-		
-		
-		
-		
-		}
 
+public class SoccerPlayer {
+	
+	String name;
+	int number;
+	String team;
+	int age;
+	
+	public static void main(String[] args) {
+		List<SoccerPlayer> player = new ArrayList();
+		
+		SoccerPlayer SocerPlayer1 =  new SoccerPlayer();
+		SoccerPlayer SocerPlayer2 =  new SoccerPlayer();
+		SoccerPlayer SocerPlayer3 =  new SoccerPlayer();
+		SoccerPlayer SocerPlayer4 = new SoccerPlayer();
+		
+		SocerPlayer1.name = "제라드";
+		SocerPlayer1.number = 8;
+		SocerPlayer1.team = "리버풀";
+		SocerPlayer1.age = 40;
+		
+		SocerPlayer2.name = "트렌드알렉산더아놀드";
+		SocerPlayer2.number = 66;
+		SocerPlayer2.team = "리버풀";
+		SocerPlayer2.age = 25;
+		
+		SocerPlayer3.name = "쿠티뉴";
+		SocerPlayer3.number = 99;
+		SocerPlayer3.team = "지옥";
+		SocerPlayer3.age = 30;
+		
+		SocerPlayer4.name = "사비알론소";
+		SocerPlayer4.number = 6;
+		SocerPlayer4.team = "리버풀";
+		SocerPlayer4.age = 40;
+		
+		player.add(SocerPlayer1);
+		player.add(SocerPlayer2);
+		player.add(SocerPlayer3);
+		player.add(SocerPlayer4);
+		
+		System.out.println(player.get(0).name+"\t" + player.get(0).number+"\t" + player.get(0).team+"\t" + player.get(0).age);
+		System.out.println();
+		System.out.println(player.get(1).name+"\t" + player.get(1).number+"\t" + player.get(1).team+"\t" + player.get(1).age);
+		System.out.println();
+		System.out.println(player.get(2).name+"\t" + player.get(2).number+"\t" + player.get(2).team+"\t" + player.get(2).age);
+		System.out.println();
+		System.out.println(player.get(3).name+"\t" + player.get(3).number+"\t" + player.get(3).team+"\t" + player.get(3).age);
+	}
 }
