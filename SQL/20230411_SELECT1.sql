ec29cdd95180ed58754bf9d7e500a9cfd213559f
hongwontae <feelchok1234@gmail.com>
2023-04-11 18:07:25 +0900

    20230411SQL

--- SQL/20230411_SELECT1.sql
+++ SQL/20230411_SELECT1.sql
@@ -1,1 +1,60 @@
-ALT+F10 => 데이터베이스 접속 선택창을 생성한다.
+-- 1.마당서점의고객이요구하는다음질문에대해SQL 문을작성하시오.
+
+-- (1) 도서번호가1인도서의이름
+
+-- (2) 가격이20,000원이상인도서의이름
+
+SELECT BOOKNAME
+FROM BOOK
+WHERE BOOKID =1;
+
+SELECT BOOKNAME
+FROM BOOK
+WHERE PRICE>=20000;
+
+SELECT BOOKNAME
+FROM BOOK
+WHERE (BOOKID =1) OR (PRICE>=20000);
+
+
+-- 2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
+
+-- (3) 모든고객의이름, 주소
+
+-- (4) 2014년7월4일~7월7일사이에주문받은도서의주문번호
+
+-- (5) 2014년7월4일~7월7일사이에주문받은도서를제외한도서의주문번호
+
+-- (6) 성이‘김’씨인고객의이름과주소
+
+-- (7) 성이‘김’씨이고이름이‘아’로끝나는고객의이름과주소
+
+SELECT NAME, ADDRESS
+FROM CUSTOMER;
+
+SELECT ORDERID
+FROM ORDERS
+WHERE ORDERDATE BETWEEN '14/07/04'AND '14/07/07';
+
+SELECT ORDERID
+FROM ORDERS
+WHERE ORDERDATE NOT BETWEEN '14/07/04'AND '14/07/07';
+
+SELECT NAME, ADDRESS
+FROM CUSTOMER
+WHERE NAME LIKE '김%';
+SELECT NAME, ADDRESS
+FROM CUSTOMER
+WHERE NAME LIKE '김__';
+
+-- (7) 성이‘김’씨이고이름이‘아’로끝나는고객의이름과주소
+SELECT NAME, ADDRESS
+FROM CUSTOMER
+WHERE NAME LIKE '김%아';
+
+
+
+
+
+
+​
