7e7a1fbc3911df798aeff6775c52b0c8cf8b5962
hongwontae <feelchok1234@gmail.com>
2023-04-24 18:17:00 +0900

    20230424_DBModelIng

--- javaproject/DeptManger/src/main/DeptManagerMain.java
+++ javaproject/DeptManger/src/main/DeptManagerMain.java
@@ -1,53 +1,90 @@
-package main;
+package service;
 
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
 
-import controller.DeptListController;
-import controller.DeptSearchController;
+import dao.DeptDao;
+import domain.Dept;
+import util.ConnectionProvider;
 
-public class DeptManagerMain {
+public class DeptListService {
+
+	DeptDao dao;
+
+	private DeptListService() {
+		this.dao = DeptDao.getInstance();
+	}
 	
-	public static Scanner sc = new Scanner(System.in);
+	private static DeptListService service = new DeptListService();
+	
+	public static DeptListService getInstance() {
+		return service;
+	}
+
+	public List<Dept> getDeptList() {
+		
+		Connection conn = null;
+		List<Dept> list = null;
+		
+		try {
+			// Connection 객체 구하기
+			//String dbUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+			//conn = DriverManager.getConnection(dbUrl, "hr", "tiger");
+			
+			conn = ConnectionProvider.getConnection();
+			
+			// 트랜젝션 시작
+			conn.setAutoCommit(false);
+			
+			// insert(conn)
+			// update(conn)
+			// select(conn)
+			// update(conn)
+			// insert(conn)
+			
+			list = dao.selectByAll(conn);
+			
+			// commit : 완료!
+			conn.commit();
+			
+		} catch (SQLException e) {
+			// 예외 발생 -> rollback
+			if(conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			
+			e.printStackTrace();
+		} finally {
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return list;
+
+	}
 	
 	public static void main(String[] args) {
 		
-		 
+		DeptListService listService = new DeptListService();
+		List<Dept> list = listService.getDeptList();
 		
-		
-		/// Controller : view -> Service -> Dao -> Service -> Controller : view
-		
-		
-		while(true) {
-			System.out.println("메뉴를 입력하세요.");
-			System.out.println("1. 부서 리스트 출력");
-			System.out.println("2. 부서 검색");
-			System.out.println("3. 부서 입력");
-			System.out.println("4. 부서 수정");
-			System.out.println("5. 부서 삭제");
-			System.out.println("6. 프로그램 종료");
-			
-			int menu = Integer.parseInt(sc.nextLine());
-			
-			switch(menu) {
-			case 1:
-				new DeptListController().getDeptList();
-				break;
-			case 2:
-				new DeptSearchController().searchDept();
-				break;
-			case 6:
-				return;
-			}
-		
+		for(Dept d : list) {
+			System.out.println(d);
 		}
 		
-		
-		
-		
-		
-		
-		
-		
 	}
-
+	
 }
